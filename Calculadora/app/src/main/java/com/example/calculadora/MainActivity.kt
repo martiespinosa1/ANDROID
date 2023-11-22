01996e5d652eeea5a1b1a04131c405ac5cb0b95b
martiespinosa1 <marti.espinosa.7e7@itb.cat>
2023-11-22 10:00:18 +0100

    calculadora funciona

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -3,13 +3,34 @@ package com.example.calculadora
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.calculadora.ui.theme.CalculadoraTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +43,86 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Body()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Body() {
+    // declara variables
+    var num1 by remember { mutableStateOf("") }
+    var num2 by remember { mutableStateOf("") }
+    var show by rememberSaveable { mutableStateOf(false) }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(24.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        Text(text = "Calculadora", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+        TextField(value = num1, onValueChange = { num1 = it }, label = { Text("Num 1") })
+        TextField(value = num2, onValueChange = { num2 = it }, label = { Text("Num 2") })
+
+
+        val op = myDropDownMenu()
+
+
+        Button(onClick = { show = true }) {
+            Text(text = "Calcular")
+        }
+
+        if (show) {
+            var result = 0
+            when {
+                op == "Suma" -> result = num1.toInt() + num2.toInt()
+                op == "Resta" -> result = num1.toInt() - num2.toInt()
+                op == "Multiplicación" -> result = num1.toInt() * num2.toInt()
+                op == "División" -> result = num1.toInt() / num2.toInt()
+            }
+
+            Text (text = "Resultado: $result", fontSize = 18.sp, fontWeight = FontWeight.Bold, color = Color.Green)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun myDropDownMenu(): String {
+    var operacion by remember { mutableStateOf("") }
+    var expanded by remember { mutableStateOf(false) }
+    val options = listOf("Suma", "Resta", "Multiplicación", "División")
+
+    OutlinedTextField(
+        value = operacion,
+        onValueChange = { operacion = it },
+        enabled = false,
+        readOnly = true,
+        modifier = Modifier
+            .clickable { expanded = true }
+            .fillMaxWidth()
+    )
+
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { expanded = false },
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        options.forEach { it ->
+            DropdownMenuItem(text = { Text(text = it) }, onClick = {
+                expanded = false
+                operacion = it
+            })
+        }
+    }
+    return operacion
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     CalculadoraTheme {
-        Greeting("Android")
+        Body()
     }
 }
