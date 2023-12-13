358ebbc959cc2c2215e0929e33a363e29fdbc21a
martiespinosa1 <marti.espinosa.7e7@itb.cat>
2023-12-13 09:46:58 +0100

    nav and splash screen done

--- HagmanApp/app/src/main/java/com/example/hagmanapp/ResultScreen.kt
+++ HagmanApp/app/src/main/java/com/example/hagmanapp/ResultScreen.kt
@@ -1,2 +1,35 @@
 package com.example.hagmanapp
 
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.hagmanapp.ui.theme.HagmanAppTheme
+
+@Composable
+fun Result(navController: NavController) {
+    Box(modifier = Modifier.fillMaxSize().background(Color.Blue)) {
+        Text(
+            text = "Result",
+            modifier = Modifier
+                .align(Alignment.Center)
+                .clickable { navController.navigate(Routes.Menu.route) })
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview4() {
+    HagmanAppTheme {
+        Result(navController = rememberNavController())
+    }
+}
--- HagmanApp/app/src/main/java/com/example/hagmanapp/SplashScreen.kt
+++ HagmanApp/app/src/main/java/com/example/hagmanapp/SplashScreen.kt
@@ -1,2 +1,69 @@
 package com.example.hagmanapp
 
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.hagmanapp.ui.theme.HagmanAppTheme
+import kotlinx.coroutines.delay
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Splash(alphaAnim: Float) {
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(painter = painterResource(id = R.drawable.logo),
+            contentDescription = "logo", alpha = alphaAnim
+        )
+        Text(text = "Welcome!", fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    var startAnimation by remember { mutableStateOf(false) }
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(durationMillis = 3000), label = ""
+    )
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
+        delay(2000)
+        navController.popBackStack()
+        navController.navigate(Routes.Menu.route)
+    }
+    Splash(alphaAnim.value)
+}
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview1() {
+    HagmanAppTheme {
+        //Splash(navController = rememberNavController())
+    }
+}
