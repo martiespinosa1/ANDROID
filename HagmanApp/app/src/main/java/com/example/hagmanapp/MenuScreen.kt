fcff8377f6e3d4bc28273f5e509bfb34d639469d
martiespinosa1 <marti.espinosa.7e7@itb.cat>
2023-12-15 10:08:39 +0100

    menu screen done

--- HagmanApp/app/src/main/java/com/example/hagmanapp/MenuScreen.kt
+++ HagmanApp/app/src/main/java/com/example/hagmanapp/MenuScreen.kt
@@ -12,7 +12,16 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
@@ -24,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -31,75 +41,134 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.hagmanapp.ui.theme.HagmanAppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Menu(navController: NavController) {
     var firstOptionSelected by remember { mutableStateOf(false) }
     var secondOptionSelected by remember { mutableStateOf(false) }
     var sliderValue by remember { mutableStateOf(0f) }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Gray)) {
+    var selectedText by remember { mutableStateOf("") }
+    var expanded by remember { mutableStateOf(false) }
+    val difficulty = listOf("Easy", "Medium", "Hard")
 
-        Box(
+    var showDialog by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.08f)
-                .align(Alignment.TopCenter)
-                .background(Color.DarkGray)) {
-            Text(
-                text = "Hangman App",
-                modifier = Modifier
-                    .padding(start = 15.dp, top = 16.dp),
-                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                .padding(bottom = 50.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "logo",
+                modifier = Modifier.requiredSize(200.dp).clip(shape = RoundedCornerShape(30.dp))
             )
         }
 
-
-
-        Column(
+        Row(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(vertical = 8.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxWidth()
+                .padding(bottom = 50.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
-            Row(
+            OutlinedTextField(
+                value = selectedText,
+                onValueChange = { selectedText = it },
+                enabled = false,
+                readOnly = true,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
+                    .clickable { expanded = true }
+            )
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Image(painter = painterResource(id = R.drawable.logo),
-                    contentDescription = "logo")
+                difficulty.forEach {
+                    DropdownMenuItem(text = { Text(text = it) }, onClick = {
+                        expanded = false
+                        selectedText = it
+                    })
+                }
             }
 
         }
 
-
-
-        Box(
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.08f)
-                .align(Alignment.BottomCenter)
-                .clickable { navController.navigate(Routes.Game.route) }
-                .background(Color.DarkGray)) {
-            Text(
-                text = "NEXT STEP",
-                modifier = Modifier
-                    .align(Alignment.Center),
-                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
-            )
+                .padding(bottom = 30.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            OutlinedButton(
+                onClick = { navController.navigate(Routes.Game.route) },
+                modifier = Modifier.requiredWidth(280.dp)
+            ){
+                Text(text = "Play", fontSize = 20.sp)
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 30.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            OutlinedButton(
+                onClick = { showDialog = true },
+                modifier = Modifier.requiredWidth(280.dp)
+            ){
+                Text(text = "Help", fontSize = 20.sp)
+            }
+            MyDialog(showDialog, { showDialog = false }) { showDialog = false}
+
         }
     }
 }
+
+
+@Composable
+fun MyDialog(show: Boolean, onDismiss: () -> Unit, function: () -> Unit) {
+    if (show) {
+        Dialog(
+            onDismissRequest = { onDismiss() },
+            properties = DialogProperties(
+                dismissOnBackPress = true,
+                dismissOnClickOutside = true,
+                usePlatformDefaultWidth = false
+            )
+        ) {
+            Column(
+                Modifier.background(Color.DarkGray)
+                    .padding(24.dp)
+            ) {
+                Text(text = "This is my dialog", color = Color.Blue)
+            }
+        }
+    }
+}
+
+
+
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
--- HagmanApp/app/src/main/java/com/example/hagmanapp/SplashScreen.kt
+++ HagmanApp/app/src/main/java/com/example/hagmanapp/SplashScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,9 +19,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
@@ -31,14 +35,17 @@ import kotlinx.coroutines.delay
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Splash(alphaAnim: Float) {
-    Column(modifier = Modifier.fillMaxSize(),
+    Column(modifier = Modifier.requiredSize(150.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Image(painter = painterResource(id = R.drawable.logo),
-            contentDescription = "logo", alpha = alphaAnim
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "logo",
+            alpha = alphaAnim,
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(20.dp))
         )
-        Text(text = "Welcome!", fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
     }
 }
 
